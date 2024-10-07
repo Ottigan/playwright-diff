ff5a92e4b8897df210b8dd7e5a9cff5612758f60
Janis Malcans <jmalcans@evolution.com>
2024-10-08 01:39:43 +0300

    Add wood texture

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-import img from "./dog.webp";
-
+import dog from "./dog.webp";
+import wood from "./wood.webp";
 
 function main() {
     const canvas = document.querySelector<HTMLCanvasElement>('canvas');
     const ctx = canvas?.getContext('2d');
     const image = new Image();
-    image.src = img;
+    image.src = dog;
 
     if (!canvas || !ctx) {
         throw new Error('Canvas error...');
     }
 
-    image.onload = () => ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    drawImage(ctx, dog, 0, 0, canvas.width);
+    drawImage(ctx, wood, 0, 250, canvas.width);
+}
+
+function drawImage(ctx: CanvasRenderingContext2D , src: string, x: number, y: number, width: number): void {
+    const image = new Image();
+    image.src = src;
+
+    image.onload = () => {
+        const aspectRatio = image.height / image.width;
+
+        console.log("aspectRatio", aspectRatio);
+        
+        const w = width / 2;
+        const h =  w * aspectRatio;
+        ctx.drawImage(image, x, y, w, h);
+    }
 }
 
 main()

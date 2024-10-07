8334298cf248edfef99afa6676319ac1fc7ea8df
Janis Malcans <jmalcans@evolution.com>
2024-10-07 19:03:38 +0300

    Update toHaveScreenshot timeout to 0

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Example");
-  await expect(page).toHaveScreenshot({ threshold: 0, maxDiffPixelRatio: 0 });
+  await expect(page).toHaveScreenshot({ timeout: 0 });
 });

9409eb29e9964b9c613071d3f8dd37393573b8d1
Janis Malcans <jmalcans@evolution.com>
2024-10-07 15:54:01 +0300

    Add initial setup

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:4173');
-
+  await page.goto("/");
   await expect(page).toHaveTitle("Example");
   await expect(page).toHaveScreenshot();
 });
